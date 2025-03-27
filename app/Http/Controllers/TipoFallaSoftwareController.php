421789ece624a025eff136c67242e6f1a246ca36
kevinRH-1 <76975127+kevinRH-1@users.noreply.github.com>
2025-03-27 11:47:49 -0400

    cambios + cambio en tiempo de sesion

--- app/Http/Controllers/TipoFallaSoftwareController.php
+++ app/Http/Controllers/TipoFallaSoftwareController.php
@@ -40,7 +40,7 @@ class TipoFallaSoftwareController extends Controller
         }else{
 
             if($vista==0){
-                $fallas = TipoFallaSoftware::where('modulo_id', $modulo)->where('activo',1)->get();
+                $fallas = TipoFallaSoftware::where('modulo_id', $modulo)->where('activo',1)->where('vista_id', null)->get();
             }else{
                 $fallas = TipoFallaSoftware::where('vista_id', $vista)->where('activo',1)->get();
             }
